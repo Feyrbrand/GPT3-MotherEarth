effe0f85065e1fdb471d20bdd9f7e76564f7d8ce
Feyrbrand <hu_dominik@hotmail.com>
2023-05-10 12:23:09 +0200

    Add files via upload
    create soundfiles with TTS conqui server

---
+++ scripts/VoiceGen.py
@@ -0,0 +1,15 @@
+import cTTS
+
+#cTTS.synthesizeToFile("./audio/greeting.wav", "Hallo Mensch! Du darfst mir drei Fragen stellen, also überlege gut.")
+
+#cTTS.synthesizeToFile("./audio/question1.wav", "Wie lautet deine erste Frage?")
+
+#cTTS.synthesizeToFile("./audio/question2.wav", "Wie lautet deine zweite Frage?")
+
+#cTTS.synthesizeToFile("./audio/question3.wav", "Wie lautet deine dritte Frage?")
+
+cTTS.synthesizeToFile("./audio/hmmmm.wav", "M. M.")
+
+#cTTS.synthesizeToFile("./audio/unclear.wav", "Ich habe dich nicht verstanden. Bitte wiederhole deine Frage.")
+
+#cTTS.synthesizeToFile("./audio/goodbye.wav", "Danke für deine Fragen. Auf Wiederhören.")
